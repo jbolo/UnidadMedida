aa3dae4bbbd869ce3b73bba2ef24d3532a15fb9c
Jonathan Bolo <jbolo.des@gmail.com>
2019-10-23 02:33:06 -0500

    Added unchecked tables

---
+++ Init.php
@@ -0,0 +1,24 @@
+<?php
+namespace FacturaScripts\Plugins\UnidadMedida;
+
+use FacturaScripts\Core\Base\InitClass;
+
+class Init extends InitClass
+{
+
+    public function init()
+    {
+        
+    }
+
+    public function update()
+    {
+        $tables = $this->toolBox()->cache()->get('fs_checked_tables');
+
+        $tables_unchecked = array('productos', 'lineasfacturascli');
+
+        $tables_checked = array_diff($tables,$tables_unchecked);
+
+        $this->toolBox()->cache()->set('fs_checked_tables', $tables_checked);
+    }
+}

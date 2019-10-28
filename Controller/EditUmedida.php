7990f9179452f3295b053194306982a929aaba26
NeoRazorX <neorazorx@gmail.com>
2019-10-28 13:24:36 +0100

    Convertidos los cambios en los modelos a extensiones.

--- Controller/EditUmedida.php
+++ Controller/EditUmedida.php
@@ -1,28 +1,46 @@
 <?php
-
+/**
+ * This file is part of the UnidadMedidas plugin for FacturaScripts
+ * Copyright (C) 2019 Jonathan Bolo <jbolo.des@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 namespace FacturaScripts\Plugins\UnidadMedida\Controller;
 
 use FacturaScripts\Core\Lib\ExtendedController\EditController;
-use FacturaScripts\Core\Base\DataBase\DataBaseWhere;
+
 /**
  * Controller to edit Umedida.
  *
  * @author Jonathan Bolo        <jbolo.des@gmail.com>
  *
  */
-class EditUmedida extends EditController 
+class EditUmedida extends EditController
 {
-    public function getModelClassName() {
+
+    public function getModelClassName()
+    {
         return 'Umedida';
     }
+
     public function getPageData()
     {
         $pageData = parent::getPageData();
-        $pageData['menu'] = 'accounting';
+        $pageData['menu'] = 'warehouse';
         $pageData['title'] = 'Unidad de Medida';
         $pageData['icon'] = 'fas fa-ruler';
 
         return $pageData;
     }
-    
 }
